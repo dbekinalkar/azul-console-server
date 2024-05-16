ba7fed14489bbba8823eef0b99fed267ccd56200
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2024-05-16 16:56:46 -0500

    Add ability for ssl certification

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import websockets
 from websockets.sync.server import ServerConnection, WebSocketServer, serve
+import ssl
 
 from player import SocketPlayer
 import game
@@ -16,6 +17,7 @@ def socket_handler(ws: ServerConnection) -> None:
 def parse_args() -> argparse.Namespace:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Azul server to host games.")
     parser.add_argument("port", nargs='?', type=int, default=3000, help="Port number (default: 3000)")
+    parser.add_argument("--cert", type=str, help="Path to the certificate file")
     args: argparse.Namespace = parser.parse_args()
     return args
 
@@ -28,8 +30,14 @@ def main() -> None:
     connections.connectionHandler = connections.ConnectionHandler()
 
     server: WebSocketServer
-    with serve(socket_handler, "", args.port) as server:
-        server.serve_forever()
+    if args.cert:
+        ssl_context: ssl.SSLContext = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ssl_context.load_cert_chain(certfile=args.cert)
+        with serve(socket_handler, "", args.port) as server:
+            server.serve_forever()
+    else:
+        with serve(socket_handler, "", args.port) as server:
+            server.serve_forever()
 
 
 if __name__ == '__main__':
